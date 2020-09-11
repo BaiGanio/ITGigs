a5ca570744c57ce63648574bbbf0be31e2204844
Lyuben Kikov <exceptionhelper@gmail.com>
2020-09-11 12:10:13 +0300

    Clean up!

---
+++ Utils/ServiceBusQueues/Program.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusQueues
+{
+    class Program
+    {
+        static QueueClient receiverQueueClient;
+        static QueueClient senderQueueClient;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SenderDoJob();
+            Task.Delay(5000);
+            ReceiverDoJob();
+        }
+
+        private static void SenderDoJob()
+        {
+            string sbConnectionString = "Endpoint=sb://free-basic.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=T+dzr/LZybbJwkJKFjr1a0uS04WZC0iCRLVVJds2p+M=";
+            string sbQueueName = "MobileRecharge";
+
+            string messageBody = string.Empty;
+            try
+            {
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine("Mobile Recharge");
+                Console.WriteLine("-------------------------------------------------------");
+                Console.WriteLine("Operators");
+                Console.WriteLine("1. Vodafone");
+                Console.WriteLine("2. Airtel");
+                Console.WriteLine("3. JIO");
+                Console.WriteLine("-------------------------------------------------------");
+
+                Console.WriteLine("Operator:");
+                string mobileOperator = Console.ReadLine();
+                Console.WriteLine("Amount:");
+                string amount = Console.ReadLine();
+                Console.WriteLine("Mobile:");
+                string mobile = Console.ReadLine();
+
+                Console.WriteLine("-------------------------------------------------------");
+
+                switch (mobileOperator)
+                {
+                    case "1":
+                        mobileOperator = "Vodafone";
+                        break;
+                    case "2":
+                        mobileOperator = "Airtel";
+                        break;
+                    case "3":
+                        mobileOperator = "JIO";
+                        break;
+                    default:
+                        break;
+                }
+
+                messageBody = mobileOperator + "*" + mobile + "*" + amount;
+                senderQueueClient = new QueueClient(sbConnectionString, sbQueueName);
+
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                Console.WriteLine($"Message Added in Queue: {messageBody}");
+                senderQueueClient.SendAsync(message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+                senderQueueClient.CloseAsync();
+            }
+        }
+
+        private static void ReceiverDoJob()
+        {
+            string sbConnectionString = "Endpoint=sb://free-basic.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=T+dzr/LZybbJwkJKFjr1a0uS04WZC0iCRLVVJds2p+M=";
+            string sbQueueName = "MobileRecharge";
+
+            try
+            {
+                receiverQueueClient = new QueueClient(sbConnectionString, sbQueueName);
+
+                var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+                {
+                    MaxConcurrentCalls = 1,
+                    AutoComplete = false
+                };
+                receiverQueueClient.RegisterMessageHandler(ReceiveMessagesAsync, messageHandlerOptions);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+                receiverQueueClient.CloseAsync();
+            }
+        }
+
+        static async Task ReceiveMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: {Encoding.UTF8.GetString(message.Body)}");
+
+            await receiverQueueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine(exceptionReceivedEventArgs.Exception);
+            return Task.CompletedTask;
+        }
+    }
+}
